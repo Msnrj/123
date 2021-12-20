e479d286b04c8064f2ee08266948ef1999e2732c
Msnrj <96429817+Msnrj@users.noreply.github.com>
2021-12-20 11:23:53 -0300

    agencia

---
+++ WebApplication1/WebApplication1/Models/pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Emal { get; set; }
+
+    }
+}
